263e7c74f1790edce2afeea9d466d16dbc955357
kimminho0428 <0428mhk@naver.com>
2024-05-14 16:04:19 +0900

    ReversedArray Test

---
+++ src/string/ReverseArray.java
@@ -0,0 +1,28 @@
+package string;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseArray {
+    public int[] solution(long n) {
+        String s = String.valueOf(n);
+        StringBuilder sb = new StringBuilder(s);
+        sb = sb.reverse();
+        String[] ss = sb.toString().split("");
+
+        int[] answer = new int[ss.length];
+        for (int i=0; i<ss.length; i++) {
+            answer[i] = Integer.parseInt(ss[i]);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ReverseArray ra = new ReverseArray();
+        System.out.println(Arrays.toString(ra.solution(12345)));
+    }
+
+
+
+
+}
--- src/string/StringArrays.java
+++ src/string/StringArrays.java
@@ -2,6 +2,14 @@ package string;
 
 public class StringArrays {
     public static void main(String[] args) {
-
+        String str1 = new String("ABC");
+        System.out.println("str1 = " + str1);
+        char[] charArray = str1.toCharArray();
+        for (char c : charArray) {
+            System.out.println("c = " + c);
+        }
+        char c = str1.charAt(2);
+        System.out.println("c = " + c);
     }
+
 }
