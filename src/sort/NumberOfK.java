e2c21f902799e20322e2e37b6032aae39ed474fe
kimminho0428 <0428mhk@naver.com>
2024-11-21 13:51:15 +0900

    정렬 - K번째 수 수정

--- src/sort/NumberOfK.java
+++ src/sort/NumberOfK.java
@@ -4,20 +4,14 @@ import java.util.Arrays;
 
 public class NumberOfK {
     public int[] solution(int[] array, int[][] commands) {
-
-
         int[] answer = new int[commands.length];
         for (int i = 0; i < answer.length; i++){
             int[] command = commands[i];
             int from = command[0] - 1;
-            System.out.println("from = " + from);
             int to = command[1];
-            System.out.println("to = " + to);
             int k = command[2] - 1;
-            System.out.println("k = " + k);
 
             int[] sub = Arrays.copyOfRange(array, from, to);
-            System.out.println("sub = " + Arrays.toString(sub));
             Arrays.sort(sub);
             answer[i] = sub[k];
         }
