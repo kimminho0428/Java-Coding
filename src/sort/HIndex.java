d56f9cb672820f4d865c5b9a8904ceeeca082f49
kimminho0428 <0428mhk@naver.com>
2024-11-28 22:28:55 +0900

    정렬 - H-INDEX 정답

--- src/sort/HIndex.java
+++ src/sort/HIndex.java
@@ -1,9 +1,27 @@
 package sort;
 
+import java.util.Arrays;
+
 public class HIndex {
+
+    private boolean isValid(int[] citations, int h){
+        int index = citations.length - h;
+        return citations[index] >= h;
+    }
+
     public int solution(int[] citations) {
-        int answer = 0;
-        return answer;
+        Arrays.sort(citations);
+        for (int h = citations.length; h >= 1; h--) {
+            if (isValid(citations, h)) return h;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        HIndex hIndex = new HIndex();
+        int[] citations = {3, 0, 6, 1, 5};
+        int solution = hIndex.solution(citations);
+        System.out.println("solution = " + solution);
     }
 
 }
