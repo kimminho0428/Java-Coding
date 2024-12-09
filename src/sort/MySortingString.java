36fe4c4b52ae5a8bf94a1d60c68e0ed37b04ec56
kimminho0428 <0428mhk@naver.com>
2024-12-09 22:13:44 +0900

    정렬 - 문자열 내 마음대로 정렬하기 정답

--- src/sort/MySortingString.java
+++ src/sort/MySortingString.java
@@ -1,10 +1,26 @@
 package sort;
 
+import java.util.Arrays;
+
 public class MySortingString {
 
     public String[] solution(String[] strings, int n) {
-        String[] answer = {};
-        return answer;
+        Arrays.sort(strings, (s1, s2) -> {
+                    if(s1.charAt(n) != s2.charAt(n)){
+                        return s1.charAt(n) - s2.charAt(n);
+                    }
+                    return s1.compareTo(s2);
+                });
+        return strings;
+    }
+
+    public static void main(String[] args) {
+        String[] strings = {"sun", "bed", "car"};
+        int n = 1;
+        MySortingString sortingString = new MySortingString();
+        String[] solution = sortingString.solution(strings, n);
+        System.out.println("solution = " + Arrays.toString(solution));
+
     }
 
 }
