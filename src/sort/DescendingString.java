f000f70d0dd1b6cecb0da24750de84c5b5d93475
kimminho0428 <0428mhk@naver.com>
2024-12-09 14:15:57 +0900

    정렬 - 문자열 내림차순으로 배치하기 정답

--- src/sort/DescendingString.java
+++ src/sort/DescendingString.java
@@ -1,10 +1,40 @@
 package sort;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DescendingString {
 
     public String solution(String s) {
-        String answer = "";
-        return answer;
+        return s.chars()
+                .boxed()
+                .sorted((v1, v2) -> v2 - v1)
+                .collect(StringBuilder::new,
+                        StringBuilder::appendCodePoint,
+                        StringBuilder::append)
+                .toString();
+
+        /**
+         * 다른 풀이
+         */
+        // char[] sol = str.toCharArray();
+        //    Arrays.sort(sol);
+        //    return new StringBuilder(new String(sol)).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        DescendingString string = new DescendingString();
+        String s = "Zbcdefg";
+        String str = s.chars()
+                .boxed()
+                .sorted((v1, v2) -> v2 - v1)
+                .collect(StringBuilder::new,
+                        StringBuilder::appendCodePoint,
+                        StringBuilder::append)
+                .toString();
+        System.out.println("str = " + str);
     }
 
 }
