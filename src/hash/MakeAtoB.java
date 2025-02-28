d40d9aeedc764b8e16368abcfdcb8dd199c8817d
kimminho0428 <0428mhk@naver.com>
2025-02-28 15:36:30 +0900

    해시 - A로 B만들기 내가 만든 코드 추가

--- src/hash/MakeAtoB.java
+++ src/hash/MakeAtoB.java
@@ -13,6 +13,21 @@ public class MakeAtoB {
         Arrays.sort(b);
 
         return new String(a).equals(new String(b)) ? 1 : 0;
+
+        /**
+         * 내가 풀어낸 방법
+         */
+      /*  int answer = 1;
+        char[] a = before.toCharArray();
+        char[] b = after.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) answer = 0;
+        }
+        return answer;
+      */
+
     }
 
     public static void main(String[] args) {
